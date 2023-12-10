85c6ba3087f19a7639bf68a406dbafd1fa19ac9f
Taylor Crowe <151951331+tcrowe14@users.noreply.github.com>
2023-12-09 17:05:29 -0700

    Update apptReports.py
    Added doc string and comments. Cleaned up. Final version.

--- apptReports.py
+++ apptReports.py
@@ -1,28 +1,42 @@
 import appointment as ap
 def show_appointments_by_name(appt_cal: list[ap.Appointment], client_name):
+    '''
+    Show appointments by name. Accepts the client name (or partial name) from user input. Checks the Appointment calendar for
+    matches. If found prints the appointment. If not prints none found.
+    Arguments:
+        Appointment calendar, Client name
+    Return Value:
+        none
+    '''
     found = False
-    for appointment in appt_cal:
-        if appointment.get_client_name().lower() == client_name.lower():
-            #If there's a match, it prints the appointment using the str() method implicitly.
+    print("Appointments for", client_name)
+    print("\n{:20s}{:15s}{:10s}{:10s}{:10s}{:20s}".format("Client Name",
+    "Phone", "Day", "Start", "End", "Type"))
+    print("-"*85)
+    for appointment in appt_cal: #For loop through calendar
+       if appointment.get_client_name().lower().startswith(client_name.lower()): #prints the appointment using the str() method.
             print(appointment)
             found = True
+    if not found:
+        print("No appointments found.")
+
 
 def show_appointments_by_day(appt_cal: list[ap.Appointment], day_of_week):
-
-
-
+    '''
+    Show appointments by day. Accepts day to search from user input. Checks the Appointment calendar for
+    matches. If found prints the appointment. If not displays blank.
+    Arguments:
+        Appointment calendar, Day of week
+    Return Value:
+        none
+    '''
     ## Displays information indicating the selected day for which appointments are being displayed.
-    print(f"Appointments for {day_of_week}\n")
-
-    #It iterates through each Appointment object in the appt_cal list. 
+    print(f"Appointments for {day_of_week}")
+    print("\n{:20s}{:15s}{:10s}{:10s}{:10s}{:20s}".format("Client Name",
+    "Phone", "Day", "Start", "End", "Type"))
+    print("-"*85)
     found = False
-    for appointment in appt_cal:
-        if appointment.get_day_of_week().lower() == day_of_week.lower():
-            #If there's a match, it prints the appointment using the str() method implicitly.
+    for appointment in appt_cal: #For loop through calendar
+        if appointment.get_day_of_week().lower() == day_of_week.lower(): #Prints the appointment using the str() method.
             print(appointment)
             found = True
-
-#If after the loop no matching appointments were found based on the given day of the week.
-#It prints a message indicating that no appointments were found for that day.
-    if not found:
-        print(f"No appointments found for {day_of_week}.\n")
